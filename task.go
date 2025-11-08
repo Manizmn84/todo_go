6c4bf75bb6835b61ae4b0bfa25bbe05265d3145e
Mani Zamani <mani.84.zmn@gmail.com>
2025-11-09 02:12:21 +0330

    feat: finish phase1

--- task.go
+++ task.go
@@ -1,6 +1,8 @@
 package qtodo
 
 import (
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -10,4 +12,56 @@ type Task interface {
 	GetAction() func()
 	GetName() string
 	GetDescription() string
+}
+
+type MyTask struct {
+	action      func()
+	alarmTime   time.Time
+	name        string
+	description string
+}
+
+func NewTask(action func(), alarm time.Time, name, desc string) (*MyTask, error) {
+	if action == nil {
+		return nil, errors.New("action can not be nil")
+	}
+
+	if alarm.IsZero() || alarm.Before(time.Now()) {
+		return nil, errors.New("alarm time must be valid and in the future")
+	}
+
+	if strings.TrimSpace(name) == "" {
+		return nil, errors.New("name can not be empty")
+	}
+
+	if strings.TrimSpace(desc) == "" {
+		return nil, errors.New("description can not be empty")
+	}
+
+	return &MyTask{
+		action:      action,
+		alarmTime:   alarm,
+		name:        name,
+		description: desc,
+	}, nil
+}
+
+func (t *MyTask) DoAction() {
+	t.action()
+}
+
+func (t *MyTask) GetAlarmTime() time.Time {
+	return t.alarmTime
+}
+
+func (t *MyTask) GetAction() func() {
+	return t.action
+}
+
+func (t *MyTask) GetName() string {
+	return t.name
+}
+
+func (t *MyTask) GetDescription() string {
+	return t.description
 }
